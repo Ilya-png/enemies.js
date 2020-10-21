1497db0ed0c34887ee654a02bdc630c89d59890d
Ilyas Jibril <jibriily2@gmail.com>
2020-10-20 22:14:39 -0400

    Initial commit

---
+++ enemies.js
@@ -0,0 +1,219 @@
+const makeBoss = function(enemy){
+  const newEnemy={
+    name:enemy.name,
+    hitPoints:enemy.attakDamage,
+    level: enemy.level,
+    alive: enemy.alive,
+    boss: true,
+  }
+  return newEnemy;
+}
+
+
+const obj= {
+  name: 'hi',
+  hitaakDamage: 15,
+  level:3,
+  alive: true,
+  boss; false,
+}
+ 
+
+const level up=function(enemy){
+  const newEnemy={
+    name:enemy.name,
+    hitPoints:enemy.alive,
+    boss:enemy.boss,
+    level:enemy.level+1,
+
+  }
+  return newEnemy;
+}
+
+const hitEnemy=function(enemy){
+  const newEnemy={
+    name:enemy.name,
+    hitPoints:enemy.hitPoints-10,
+
+  }
+
+
+if (newEnemy.hitPoints<=0){
+  newEnemy.alive=false
+
+}
+return newEnemy;
+}
+
+
+
+if { you reaal wna to t fjeiht }( at thit pooint ot fn aindirt )
+
+
+if(my anme is Ilyas )
+return: true;
+else( rerurn) false;
+
+
+
+const beastModeAtl= function(enemey){
+  const supered =makeSuper(enemy);
+  const bossed makeBoss(supered);
+  const newEnemy=buff(leveledup);
+
+  return newEnemy;
+}
+
+const beastModelAt2=function(enemy){
+  return buff(levelUp)(makeBoss(makesuper(enemy))))
+
+}
+
+beastModaAlt(obj);
+
+
+const beastMode=function(enemy){
+  const newEnemy = {
+    hitPoints: enemy.hitPoint, 
+    alive: enemy.alive,
+    name: "Spuer"+enemy.name,
+    attacDamage:enemy.attackDamage+15,
+    boss: true,
+  }
+
+  return newEnemy;
+}
+
+const obk1={
+  name:'Colin',
+  age: 40,
+}
+
+
+
+const obj2={
+  age: 40,
+  name: 'Colin',
+}
+
+const obj={lovesKeyboards: true}
+const obj2={name: 'Colin'}
+const obj3=object.assin(obj1, objs)
+obj3{loveskeyboards: true,name: 'Colin', 'Colin', age; 40}
+obj1 { lovesKeyboards:true}
+
+const makeBoss= function(enemy){
+  const newenemy= =function(enemy)
+  const newEnemy=Object.assign()
+  return {
+    name: enemy.name,
+    hitPoints: enemy.hitPoints,
+    level: enemy.level,
+    alive:enemy.alive,
+    boss: true,
+  }
+}
+
+
+const enemy1={
+  name; 'Goomba',
+  attackDamage: 5,
+  boss: false, 
+  level: 1,
+  alive: true,
+  hitPoints:100,
+}
+
+
+const makeEnemy=function(name, hitPoint, attackDamage, level, alive)
+name: name, 
+hitPointshitPoints,
+attackDamage: attackDamage,
+level: level,
+alive: alive,
+
+makeBoss(enemy)
+
+
+const makeBoss= function(enemy){
+  return {...enemy,.. {boss:true}}
+}
+
+
+makeEnemy('colin', 100, 9001, 3, false)//? {name: 'Colin, hitPoint'}
+
+
+makeEnemy('Colin', 100, 9001) //?(name: 'Colin', hitPoints: 100,)
+
+{name: 'Collin', hitPoint:100 attackDamage: 9001,
+level:1}
+
+const makeEnemy=function(name, hitPoints, attackDamaage){
+  return{
+    name:name,
+    hitPoints:hitPoints,
+    attackDamage: attackDamage,
+    level:1,
+    alive: true,
+  }
+}
+
+
+const enemies=[]
+let enemyCount=0;
+while (enemyCount<100){
+  enemey.push(makeenemy('Goomba',100, 1))
+  enemyCount++
+}
+
+
+
+const enemy1={
+  name;' Goomba',
+  element: 'wind',
+
+}
+ const makeshark=function(name, species, length){
+   return{
+     name: name,
+     socies: species,
+     length: newlength,
+     carnivore: true,
+
+   }
+ }
+
+ const tBone=makeShark('T-Bone', 'great white', 20);
+ tBone;
+
+ const x=y;
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
